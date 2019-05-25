47574aeef6dfd51fcde235d156835c1f82cb3334
Pedro Mietto Bruini <pedro.bruini@hotmail.com>
2019-05-25 20:49:54 -0300

    Add projects

---
+++ AngelHack.DbMock/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AngelHack.DbMock
+{
+    public class Class1
+    {
+    }
+}
---
+++ AngelHack.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AngelHack.Repository
+{
+    public class Class1
+    {
+    }
+}
