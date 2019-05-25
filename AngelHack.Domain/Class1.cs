8b2f06160d85ba635c631577a46fce5c606bdbee
Pedro Mietto Bruini <pedro.bruini@hotmail.com>
2019-05-25 20:52:14 -0300

    Clear Solutions

--- AngelHack.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AngelHack.Domain
-{
-    public class Class1
-    {
-    }
-}
--- AngelHack.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AngelHack.Repository
-{
-    public class Class1
-    {
-    }
-}
