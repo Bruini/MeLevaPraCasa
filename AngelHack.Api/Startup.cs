7e548324dce83b6ea490c4bdba4280da123502f3
Pedro Mietto Bruini <pedro.bruini@hotmail.com>
2019-05-25 21:32:04 -0300

    Swagger Configuration

--- AngelHack.Api/Startup.cs
+++ AngelHack.Api/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AngelHack.Api
 {
@@ -26,6 +29,31 @@ namespace AngelHack.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "AngelHack",
+                        Version = "v1",
+                        Description = "API da MeLevaPraCasa - AngelHack",
+                        Contact = new Contact
+                        {
+                            Name = "MeLevaPraCasa",
+                            Url = "https://github.com/Bruini/MeLevaPraCasa"
+                        }
+                    });
+
+                // Usar a documentação XML dos métodos.
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +70,13 @@ namespace AngelHack.Api
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vicelulas API V1");
+            });
+
         }
     }
 }
